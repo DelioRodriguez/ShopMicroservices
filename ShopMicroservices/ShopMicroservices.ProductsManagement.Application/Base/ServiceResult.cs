ff1319e97486112977513d723924a7dc2492c0d7
deliorodriguez <deliolorenzo28@gmail.com>
2024-07-16 09:28:46 -0400

    .

---
+++ ShopMicroservices/ShopMicroservices.ProductsManagement.Application/Base/ServiceResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopMicroservices.ProductsManagement.Application.Base
+{
+    internal class ServiceResult
+    {
+    }
+}
