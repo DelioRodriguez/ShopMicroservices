8981c5a5664c4e877edd8321346f6222dab0ff60
DelioRodriguez <149604497+DelioRodriguez@users.noreply.github.com>
2024-07-16 09:31:32 -0400

    Update BaseEntity.cs

--- ShopMicroservices/ShopMicroservices.common.Data/Base/BaseEntity.cs
+++ ShopMicroservices/ShopMicroservices.common.Data/Base/BaseEntity.cs
@@ -6,16 +6,8 @@ using System.Threading.Tasks;
 
 namespace ShopMicroservices.Common.Data.Base
 {
-    public abstract class AuditEntity<TType>: BaseEntity<TType>
+    public abstract class BaseEntity <TType>
     {
-
-        public DateTime? modify_date { set; get; }
-        public int? modify_user { set; get; }
-        public int creation_user { set; get; }
-        public DateTime creation_date { set; get; }
-        public int? delete_user { set; get; }
-        public DateTime? delete_date { set; get; }
-        public bool deleted { set; get; }
-
+        public abstract TType Id { get; set; }
     }
 }
