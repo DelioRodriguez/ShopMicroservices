035c9bb1aca9bd9e359fca1bc5493a62a8e9d4c9
DelioRodriguez <149604497+DelioRodriguez@users.noreply.github.com>
2024-07-16 09:32:18 -0400

    Update RepositoryBase.cs

--- ShopMicroservices/ShopMicroservices.common.Data/Repository/RepositoryBase.cs
+++ ShopMicroservices/ShopMicroservices.common.Data/Repository/RepositoryBase.cs
@@ -4,9 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ShopMicroservices.common.Data.Repository
+namespace ShopMicroservices.Common.Data.Repository
 {
-    internal class RepositoryBase
+   public interface IRepositoryBase<TEntity,TType> where TEntity : class
     {
+        List<TEntity> GetAll();
+        TEntity GetEntityByID(TType id);
+        void Save(TEntity entity);
+        void Update(TEntity entity);
+        void Remove(TEntity entity);
+
+        bool Exists(Expression<Func<TEntity, bool>> filter);
     }
 }
